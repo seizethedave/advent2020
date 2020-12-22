299748b79f6a84288fe2a766b68db1bb78fa7689
David Grant <david.grant@mixpanel.com>
2020-12-21 21:49:20 -0800

    declutter

--- advent05/05a.cpp
+++ advent05/05a.cpp
@@ -7,28 +7,16 @@
 
 const char* Filename = "advent05.txt";
 
-int seatNumber(const std::string& row, const std::string& col) {
-    uint8_t rowNum = 0;
-    uint8_t pow = 1 << 6;
-
-    for (char const &c: row) {
-        if (c == 'B') {
-            rowNum |= pow;
+uint8_t decode(const std::string& str, char signalChar) {
+    uint8_t result = 0;
+    uint8_t pow = 1 << (str.length() - 1);
+    for (const char &c: str) {
+        if (c == signalChar) {
+            result |= pow;
         }
         pow >>= 1;
     }
-
-    uint8_t colNum = 0;
-    pow = 1 << 2;
-
-    for (char const &c: col) {
-        if (c == 'R') {
-            colNum |= pow;
-        }
-        pow >>= 1;
-    }
-
-    return int(rowNum) * 8 + int(colNum);
+    return result;
 }
 
 int main(int argc, char** argv) {
@@ -39,9 +27,9 @@ int main(int argc, char** argv) {
     while (std::getline(input, line)) {
         std::string row(line, 0, 7);
         std::string col(line, 7, 3);
-        int num = seatNumber(row, col);
-        std::cout << num << std::endl;
-        highest = std::max(highest, num);
+
+        int seatNumber = int(decode(row, 'B')) * 8 + int(decode(col, 'R'));
+        highest = std::max(highest, seatNumber);
     }
 
     std::cout << "highest = " << highest << std::endl;
