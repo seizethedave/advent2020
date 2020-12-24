f45b282ac5a8b628be85bdab81de6a32b4c29c7b
David Grant <david.grant@mixpanel.com>
2020-12-24 10:05:34 -0800

    13a

---
+++ advent13/13a.py
@@ -0,0 +1,10 @@
+import sys
+
+start = int(sys.stdin.readline())
+buses = [int(c) for c in sys.stdin.readline().split(",") if c != "x"]
+
+def wait_time(b):
+    return -(start % b) + b
+
+bus = min(buses, key=wait_time)
+print(bus * wait_time(bus))
