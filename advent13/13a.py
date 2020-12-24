3743feb4ba62aa01d39b7ec780e3c4e5d0c8e3bf
David Grant <david.grant@mixpanel.com>
2020-12-24 10:09:53 -0800

    simpler

--- advent13/13a.py
+++ advent13/13a.py
@@ -4,7 +4,7 @@ start = int(sys.stdin.readline())
 buses = [int(c) for c in sys.stdin.readline().split(",") if c != "x"]
 
 def wait_time(b):
-    return -(start % b) + b
+    return b - (start % b)
 
 bus = min(buses, key=wait_time)
 print(bus * wait_time(bus))
