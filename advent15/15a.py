0c14d1bfdd7e4af6d3d6539b11a2af17633639b8
David Grant <david.grant@mixpanel.com>
2020-12-26 13:53:13 -0800

    15a as generator.

--- advent15/15a.py
+++ advent15/15a.py
@@ -1,30 +1,28 @@
 from collections import defaultdict
 
-last_seen = defaultdict(list)
-latest = 0
-ct = 0
+def iter_numbers():
+    starter = [9, 12, 1, 4, 17, 0, 18]
+    last_seen = defaultdict(list)
+    latest = 0
+    ct = 0
 
-starter = [9, 12, 1, 4, 17, 0, 18]
-
-def next_number():
-    global latest, ct, starter
-
-    if starter:
-        n, starter = starter[0], starter[1:]
-    else:
-        n = latest
-        seen_list = last_seen[n]
-        if len(seen_list) <= 1:
-            n = 0
+    while True:
+        if starter:
+            n, starter = starter[0], starter[1:]
         else:
-            n = seen_list[-1] - seen_list[-2]
+            n = latest
+            seen_list = last_seen[n]
+            if len(seen_list) <= 1:
+                n = 0
+            else:
+                n = seen_list[-1] - seen_list[-2]
 
-    last_seen[n].append(ct)
-    latest = n
-    ct += 1
-    return n
+        last_seen[n].append(ct)
+        latest = n
+        ct += 1
+        yield n
 
-
-while ct < 2020:
-    n = next_number()
-print(n)
+for i, n in enumerate(iter_numbers(), start=1):
+    if i == 2020:
+        print(n)
+        break
