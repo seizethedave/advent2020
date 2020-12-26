49cd5c296b246fe797582e1f65ab01401f6447e6
David Grant <david.grant@mixpanel.com>
2020-12-26 15:02:19 -0800

    less codez

--- advent15/a15lib.py
+++ advent15/a15lib.py
@@ -1,11 +1,11 @@
 from collections import defaultdict
+import itertools
 
 def iter_numbers():
     starter = [9, 12, 1, 4, 17, 0, 18]
     last_seen = defaultdict(list)
-    ct = 0
 
-    while True:
+    for ct in itertools.count():
         if starter:
             n, starter = starter[0], starter[1:]
         else:
@@ -16,5 +16,4 @@ def iter_numbers():
                 n = 0
 
         last_seen[n].append(ct)
-        ct += 1
         yield n
