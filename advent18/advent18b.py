03f4363046729c3392bbdd529a0cbd1cdaf984c6
David Grant <david.grant@mixpanel.com>
2020-12-28 17:05:17 -0800

    18b

---
+++ advent18/advent18b.py
@@ -0,0 +1,47 @@
+import sys
+import operator
+
+def eval_expr(expr):
+    return eval_expr_iter(iter(expr))
+
+operators = {'*': operator.mul, '+': operator.add}
+precedence = {operator.mul: 1, operator.add: 2}
+
+def eval_expr_iter(it):
+    operands = []
+    opstack = []
+
+    for c in it:
+        if c.isspace():
+            continue
+        elif c.isdecimal():
+            operands.append(int(c))
+        elif c in operators:
+            op = operators[c]
+            try:
+                lastop = opstack[-1]
+            except IndexError:
+                pass
+            else:
+                if precedence[lastop] >= precedence[op]:
+                    # Go ahead and eval lastop in place.
+                    opstack.pop()
+                    operands.append(lastop(operands.pop(), operands.pop()))
+            opstack.append(op)
+        elif c == '(':
+            operands.append(eval_expr_iter(it))
+
+        if c == ')':
+            break
+
+    while opstack:
+        op = opstack.pop()
+        operands.append(op(operands.pop(), operands.pop()))
+
+    return operands[0]
+
+if __name__ == "__main__":
+    total = 0
+    for line in sys.stdin:
+        total += eval_expr(line.rstrip())
+    print(total)
