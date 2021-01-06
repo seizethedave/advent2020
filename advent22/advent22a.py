d20a7e166287be83a3bb65dfb43b1f42d2c9b077
David Grant <david.grant@mixpanel.com>
2021-01-05 22:04:25 -0800

    22a

---
+++ advent22/advent22a.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+def get_decks():
+    def read_one_deck():
+        sys.stdin.readline()
+        d = deque()
+        for line in sys.stdin:
+            line = line.strip()
+            if not line:
+                break
+            d.append(int(line))
+        return d
+
+    return read_one_deck(), read_one_deck()
+
+d1, d2 = get_decks()
+
+while d1 and d2:
+    c1, c2 = d1.popleft(), d2.popleft()
+    assert c1 != c2
+    if c1 > c2:
+        d1.append(c1)
+        d1.append(c2)
+    else:
+        d2.append(c2)
+        d2.append(c1)
+
+def score_deck(deck):
+    return sum((len(deck) - i) * c for i, c in enumerate(deck))
+
+print(score_deck(d1 or d2))
